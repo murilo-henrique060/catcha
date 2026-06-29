a4dbc0887317e0910f8d8ef32e05025c23fe8cf3
murilo-henrique060 <henrique.henryconde@gmail.com>
2026-06-29 11:38:20 -0300

    Added Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "@/lib/contexts/UserContext";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
           {children}
         </UserProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
